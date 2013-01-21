e34ff8ddab23865fc72a924f657784b683579dac
KouichiYamada <moguo_50000_nyanko@yahoo.co.jp>
2013-01-22 02:03:55 +0900

    cnapter 2 files add

---
+++ chapter2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+'''
+Try for emulating SICP Chapter 2 source.
+'''
+
+def liner_combination(a, b, x, y):
+	'''
+	P.45
+	'''
+	return a * x + b * y
+
+def add_rat(x, y):
+	'''
+	P.47
+	'''
+	pass
+
+if __name__ == '__main__':
+	print(__file__+" is loaded.")
---
+++ test_chapter2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import unittest
+import fractions
+import math
+
+from chapter2 import *
+
+class TestChapter2(unittest.TestCase):
+	'''
+	Test class for SICP chapter 2 emulate functions.
+	'''
+	pass
+
+if __name__ == '__main__':
+	print(__file__)
+	unittest.main()
