bb37180886bd29335dd5ffcd304c85980043ce4a
KouichiYamada <moguo_50000_nyanko@yahoo.co.jp>
2013-01-23 03:21:58 +0900

    rational functions add

--- chapter2.py
+++ chapter2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import chapter1 as cp1
+
 '''
 Try for emulating SICP Chapter 2 source.
 '''
@@ -11,11 +13,65 @@ def liner_combination(a, b, x, y):
 	'''
 	return a * x + b * y
 
+def make_rat(n, d):
+	'''
+	P.48
+	'''
+	return (n, d)
+	#g = cp1.gcd(n, d)
+	#return (n/g, d/g)
+	
+def numer(x):
+	'''
+	P.48
+	'''
+	g = cp1.gcd(x[0], x[1])
+	return x[0]/g
+	#return x[0]
+
+def denom(x):
+	'''
+	P.48
+	'''
+	g = cp1.gcd(x[0], x[1])
+	return x[1]/g
+	#return x[1]
+
 def add_rat(x, y):
 	'''
 	P.47
 	'''
-	pass
+	return make_rat(numer(x)*denom(y) + numer(y)*denom(x), denom(x)*denom(y))
+
+def sub_rat(x, y):
+	'''
+	P.47
+	'''
+	return make_rat(numer(x)*denom(y) - numer(y)*denom(x), denom(x)*denom(y))
+
+def mul_rat(x, y):
+	'''
+	P.47
+	'''
+	return make_rat(numer(x)*numer(y), denom(x)*denom(y))
+
+def div_rat(x, y):
+	'''
+	P.47
+	'''
+	return make_rat(numer(x)*denom(y), denom(x)*numer(y))
+
+def equal_rat(x, y):
+	'''
+	P.47
+	'''
+	return numer(x)*denom(x) == numer(y)*denom(y)
+
+def print_rat(x):
+	'''
+	P.48
+	'''
+	print(str(numer(x))+"/"+str(denom(x)))
 
 if __name__ == '__main__':
 	print(__file__+" is loaded.")
--- test_chapter2.py
+++ test_chapter2.py
@@ -11,7 +11,26 @@ class TestChapter2(unittest.TestCase):
 	'''
 	Test class for SICP chapter 2 emulate functions.
 	'''
-	pass
+	def test_add_rat(self):
+		one_half = make_rat(1, 2)
+		#print_rat(one_half)
+		one_third = make_rat(1, 3)
+		#print_rat(one_third)
+		res = add_rat(one_half, one_third)
+		self.assertEqual(make_rat(5, 6), res)
+
+	def test_mul_rat(self):
+		one_half = make_rat(1, 2)
+		one_third = make_rat(1, 3)
+		res = mul_rat(one_half, one_third)
+		self.assertEqual(make_rat(1, 6), res)
+
+	def test_add_rat_simplification(self):
+		one_third = make_rat(1, 3)
+		res = add_rat(one_third, one_third)
+		#self.assertEqual(make_rat(2, 3), res)
+		self.assertEqual(numer(res), 2)
+		self.assertEqual(denom(res), 3)
 
 if __name__ == '__main__':
 	print(__file__)
